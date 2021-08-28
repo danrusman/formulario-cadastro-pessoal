fef7dbe04b989ca749112404cd2aa99a363e61d3
danrusman <darman0551@gmail.com>
2021-08-28 18:38:56 -0300

    Update validacao.js

--- js/validacao.js
+++ js/validacao.js
@@ -6,6 +6,33 @@ function validar() {
     var telefoneCelular = document.getElementById("telefoneCelular");
     var termo = document.getElementById("termo");
 
+
+    /*Validação de CPF - Inicio*/
+    function TestaCPF(strCPF) {
+        var Soma;
+        var Resto;
+        Soma = 0;
+        if (strCPF == "00000000000") return false;
+
+        for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+        Soma = 0;
+        for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+        return true;
+    }
+    var strCPF = "12345678909";
+    alert(TestaCPF(strCPF));
+    /*Validação de CPF - Fim*/
+
+    /*Validação de campos obrigatórios - Inicio*/
     if (nome.value == "") {
         alert("É obrigatório informar o nome.");
         nome.focus();
@@ -41,6 +68,8 @@ function validar() {
         termo.focus();
         return;
     }
+    /*Validacao de campos obrigatorios - Fim*/
+
 
     alert("Formulário enviado com sucesso!");
 }
