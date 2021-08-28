903c7172ba6fc64dc1ff22bb8927521b4246113c
danrusman <darman0551@gmail.com>
2021-08-28 18:30:19 -0300

    Update validacao.js

--- js/validacao.js
+++ js/validacao.js
@@ -1,5 +1,4 @@
 function validar() {
-    // pegando o valor do nome pelos id
     var nome = document.getElementById("nome");
     var cpf = document.getElementById("cpf");
     var endereco = document.getElementById("endereco");
@@ -7,41 +6,41 @@ function validar() {
     var telefoneCelular = document.getElementById("telefoneCelular");
     var termo = document.getElementById("termo");
 
-  // verificar se o nome está vazio
     if (nome.value == "") {
         alert("É obrigatório informar o nome.");
-
-        // Deixa o input com o focus
         nome.focus();
-        // retorna a função e não olha as outras linhas
         return;
     }
 
     if (cpf.value == "") {
         alert("É obrigatório informar o CPF.");
         cpf.focus();
+        return;
     }
 
     if (endereco.value == "") {
         alert("É obrigatório informar o endereço.")
         endereco.focus();
+        return;
     }
 
     if (numero.value == "") {
         alert("É obrigatório informar o número de endereço.")
         numero.focus();
+        return;
     }
 
     if (telefoneCelular.value == "") {
         alert("É obrigatório informar o telefone de celular.")
         telefoneCelular.focus();
+        return;
     }
 
     if (termo.value == "") {
         alert("É preciso estar ciente com o Termo de Política de Privacidade e com a LGPD vigentes.")
         termo.focus();
+        return;
     }
+
     alert("Formulário enviado com sucesso!");
-    // envia o formulário
-    //formulario.submit();
 }
